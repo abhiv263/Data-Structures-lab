eb1f97fc5ae8d840de67f6460f0f869df35b26f5
abhiv263 <abhiverma2637@gmail.com>
2025-04-08 12:22:58 +0530

    Add files via upload

---
+++ EXPERIMENT2.c
@@ -0,0 +1,46 @@
+// WRITE A PROGRAM TO PERFORM INSERTION SORT.//
+
+#include <stdio.h>
+
+void insertionSort(int arr[], int n);
+void displayArray(int arr[], int n);
+
+int main() {
+    int arr[100], n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    insertionSort(arr, n);
+
+    printf("Sorted array: ");
+    displayArray(arr, n);
+
+    return 0;
+}
+
+void insertionSort(int arr[], int n) {
+    int i, key, j;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+void displayArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
